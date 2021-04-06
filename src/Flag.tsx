9a901900c2dbd1a548b749d10bbada1f93920a06
Nguyen Trong Viet <vietnt.uet@gmail.com>
2021-04-06 23:50:49 +0700

    Update Flag.tsx

--- src/Flag.tsx
+++ src/Flag.tsx
@@ -1,99 +1,52 @@
-import React, { memo } from 'react'
-import { Emoji } from './Emoji'
-import { CountryCode } from './types'
-import { useContext } from './CountryContext'
-import { useAsync } from 'react-async-hook'
-import {
-  Image,
-  StyleSheet,
-  PixelRatio,
-  Text,
-  View,
-  ActivityIndicator,
-} from 'react-native'
-
+import React, { memo } from 'react';
+import { Emoji } from './Emoji';
+import { useContext } from './CountryContext';
+import { useAsync } from 'react-async-hook';
+import { Image, StyleSheet, PixelRatio, Text, View, ActivityIndicator, } from 'react-native';
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 30,
-    marginRight: 10,
-  },
-  emojiFlag: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1 / PixelRatio.get(),
-    borderColor: 'transparent',
-    backgroundColor: 'transparent',
-  },
-  imageFlag: {
-    resizeMode: 'contain',
-    width: 25,
-    height: 19,
-    borderWidth: 1 / PixelRatio.get(),
-    opacity: 0.8,
-  },
-})
-
-interface FlagType {
-  countryCode: CountryCode
-  withEmoji?: boolean
-  withFlagButton?: boolean
-  flagSize: number
-}
-
-const ImageFlag = memo(({ countryCode, flagSize }: FlagType) => {
-  const { getImageFlagAsync } = useContext()
-  const asyncResult = useAsync(getImageFlagAsync, [countryCode])
-  if (asyncResult.loading) {
-    return <ActivityIndicator size={'small'} />
-  }
-  return (
-    <Image
-      resizeMode={'contain'}
-      style={[
-        styles.imageFlag,
-        { borderColor: 'transparent', height: flagSize },
-      ]}
-      source={{ uri: asyncResult.result }}
-    />
-  )
-})
-
-const EmojiFlag = memo(({ countryCode, flagSize }: FlagType) => {
-  const { getEmojiFlagAsync } = useContext()
-  const asyncResult = useAsync(getEmojiFlagAsync, [countryCode])
-
-  if (asyncResult.loading) {
-    return <ActivityIndicator size={'small'} />
-  }
-  return (
-    <Text
-      style={[styles.emojiFlag, { fontSize: flagSize }]}
-      allowFontScaling={false}
-    >
-      <Emoji {...{ name: asyncResult.result! }} />
-    </Text>
-  )
-})
-
-export const Flag = ({
-  countryCode,
-  withEmoji,
-  withFlagButton,
-  flagSize,
-}: FlagType) =>
-  withFlagButton ? (
-    <View style={styles.container}>
-      {withEmoji ? (
-        <EmojiFlag {...{ countryCode, flagSize }} />
-      ) : (
-        <ImageFlag {...{ countryCode, flagSize }} />
-      )}
-    </View>
-  ) : null
-
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 30,
+        marginRight: 10,
+    },
+    emojiFlag: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1 / PixelRatio.get(),
+        borderColor: 'transparent',
+        backgroundColor: 'transparent',
+    },
+    imageFlag: {
+        resizeMode: 'cover',
+        width: 30,
+        height: 30,
+        borderRadius: 30
+    },
+});
+const ImageFlag = memo(({ countryCode, flagSize }) => {
+    const { getImageFlagAsync } = useContext();
+    const asyncResult = useAsync(getImageFlagAsync, [countryCode]);
+    if (asyncResult.loading) {
+        return React.createElement(ActivityIndicator, { size: 'small' });
+    }
+    return (React.createElement(Image, { resizeMode: 'cover', style: [
+            styles.imageFlag,
+            { borderColor: 'transparent',backgroundColor: 'red', height: flagSize, width: flagSize, borderRadius: flagSize },
+        ], source: { uri: asyncResult.result } }));
+});
+const EmojiFlag = memo(({ countryCode, flagSize }) => {
+    const { getEmojiFlagAsync } = useContext();
+    const asyncResult = useAsync(getEmojiFlagAsync, [countryCode]);
+    if (asyncResult.loading) {
+        return React.createElement(ActivityIndicator, { size: 'small' });
+    }
+    return (React.createElement(Text, { style: [styles.emojiFlag, { fontSize: flagSize }], allowFontScaling: false },
+        React.createElement(Emoji, Object.assign({}, { name: asyncResult.result }))));
+});
+export const Flag = ({ countryCode, withEmoji, withFlagButton, flagSize, }) => withFlagButton ? (React.createElement(View, { style: styles.container }, withEmoji ? (React.createElement(EmojiFlag, Object.assign({}, { countryCode, flagSize }))) : (React.createElement(ImageFlag, Object.assign({}, { countryCode, flagSize }))))) : null;
 Flag.defaultProps = {
-  withEmoji: true,
-  withFlagButton: true,
-}
+    withEmoji: true,
+    withFlagButton: true,
+};
+//# sourceMappingURL=Flag.js.map
